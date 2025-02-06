97f563be0f236a5b100b6657549e1d3e1cc44fb7
xiexin12138 <xiexin344@163.com>
2025-02-06 21:32:58 +0800

    更新 title，隐藏 github 标识

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "DeepSeek AI",
+  description: "DeepSeek R1",
   appleWebApp: {
-    title: "NextChat",
+    title: "DeepSeek AI",
     statusBarStyle: "default",
   },
 };
